506691a1e53ed7dce08d9b57335f5863827d9c16
Gerardo Castillo <gerrcass@gmail.com>
2024-06-22 18:02:09 -0400

    Update: Add styles to floating navigation and functionality on scroll

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -7,7 +7,7 @@ import { PinContainer } from "./ui/Pin";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <section className="py-20" id="projects">
       <h1 className="heading">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
@@ -69,7 +69,7 @@ const RecentProjects = () => {
           </div>
         ))}
       </div>
-    </div>
+    </section>
   );
 };
 
