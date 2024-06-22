beff482288885ea2c50c067e6683287312cd7538
Gerardo Castillo <gerrcass@gmail.com>
2024-06-22 09:21:46 -0400

    feat: Add Navigation Menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="text-3xl">
+      <FloatingNav navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]} />
       <Hero />
     </main>
   );
